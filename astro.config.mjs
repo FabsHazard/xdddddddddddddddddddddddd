8236cf09c9204fab2f2a9bf71a70669a880a47a6
FabsHazard <fcriveros@uc.cl>
2023-11-19 04:41:04 -0300

    fix username

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://FasbsHazard.github.io',
+  site: 'https://FabsHazard.github.io',
   base: '/MetchainWebsite',
 });
