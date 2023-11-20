c00d99ff88749e7687cb409a0cc855ea224f98ef
FabsHazard <fcriveros@uc.cl>
2023-11-20 18:11:40 -0300

    gui dix

--- src/Components/Divider.jsx
+++ src/Components/Divider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Divider = () => (
-  <div className="w-full lg:w-3/5 mx-auto">
+  <div className="w-full lg:w-3/5 mx-auto mt-16 mb-16">
     <div className="border-t border-purple-500 "></div>
   </div>
 );
