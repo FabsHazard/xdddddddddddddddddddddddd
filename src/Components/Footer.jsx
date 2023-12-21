46d6975b5e74c55db255305528b5ac953fd424d6
FabsHazard <fcriveros@uc.cl>
2023-12-21 20:16:38 -0300

    Hero Met

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,23 @@
-import { FacebookIcon } from "../assets/icons/FacebookIcon";
-import { InstagramIcon } from "../assets/icons/InstagramIcon";
-import { TwitterIcon } from "../assets/icons/TwitterIcon";
 import React from 'react';
 
 const footerData = [
   {
-    title: "Rsources",
-    items: ["White paper", "Explorer"],
+    title: "Resources",
+    items: [
+      { label: "White paper", link: "https://metchain.tech/whitepaper" },
+      { label: "Explorer", link: "https://www.metscan.io/" },
+    ],
   },
   {
     title: "About",
-    items: ["Team"],
+    items: [{ label: "Team", link: "https://metchain.tech/team" }],
   },
 ];
 
 export const Footer = () => {
   return (
     <footer>
-      <div className="pt-10  lg:pt-20 lg:pb-12 bg-customDarkBg1 radius-for-skewed ">
+      <div className="pt-10 lg:pt-20 lg:pb-12 bg-customDarkBg1 radius-for-skewed">
         <div className="container mx-auto px-4 w-4/5 md:w-11/12 lg:w-10/12 xl:w-4/5 2xl:w-2/3">
           <div className="flex flex-wrap">
             <div className="w-full lg:w-1/3 mb-16 lg:mb-0">
@@ -27,11 +27,11 @@ export const Footer = () => {
                 </div>
               </div>
               <p className="mb-10 mt-4 sm:w-[22rem] lg:w-[20rem] xl:w-[24rem] text-gray-400 leading-loose text-center lg:text-left mx-auto lg:mx-0">
-              Building trust, block by block.
-               Empowering the future with MetChain - Where innovation meets blockchain technology.
+                Building trust, block by block.
+                Empowering the future with MetChain - Where innovation meets blockchain technology.
               </p>
             </div>
-            <div className="w-full lg:w-2/3  lg:pl-16 hidden lg:flex flex-wrap justify-between">
+            <div className="w-full lg:w-2/3 lg:pl-16 hidden lg:flex flex-wrap justify-between">
               <div className="w-full md:w-1/3 lg:w-auto mb-16 md:mb-0">
                 <h3 className="mb-6 text-2xl font-bold text-white">Resources</h3>
                 <ul>
@@ -39,10 +39,12 @@ export const Footer = () => {
                     <li key={i} className="mb-4">
                       <a
                         className="text-gray-400 hover:text-gray-300"
-                        href="#"
-                        aria-label=""
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={item.label}
                       >
-                        {item}
+                        {item.label}
                       </a>
                     </li>
                   ))}
@@ -57,10 +59,12 @@ export const Footer = () => {
                     <li key={i} className="mb-4">
                       <a
                         className="text-gray-400 hover:text-gray-300"
-                        href="#"
-                        aria-label=""
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={item.label}
                       >
-                        {item}
+                        {item.label}
                       </a>
                     </li>
                   ))}
@@ -68,9 +72,9 @@ export const Footer = () => {
               </div>
             </div>
           </div>
-            <p className="lg:text-center text-sm text-gray-400  pt-12 mt-16 hidden lg:block">
-              &copy; Copyright ©2023 Metchain. All rights reserved.
-            </p>
+          <p className="lg:text-center text-sm text-gray-400 pt-12 mt-16 hidden lg:block">
+            &copy; Copyright ©2023 Metchain. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
