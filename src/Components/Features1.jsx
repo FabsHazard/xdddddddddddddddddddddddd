ee0f43ce82e773bbee3000a75d3f3dd2e78c9946
FabsHazard <fcriveros@uc.cl>
2024-03-24 16:16:04 -0300

    Metchain

--- src/Components/Features1.jsx
+++ src/Components/Features1.jsx
@@ -9,103 +9,94 @@ import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
 import { LoadingIcon } from "../assets/icons/LoadingIcon";
 import background1 from "../assets/images/Background1.png";
 import "../styles/gradient.css";
+
 export const Features1 = () => {
   return (
     <section
-      className="w-full bg-customDarkBg2 mt-20 mb-8 sm:mt-16 sm:mb-16 xl:mt-0  xl:m pt-[2rem]  md:pt-[12vw] lg:pt-0"
+      className="w-full bg-customDarkBg2 mt-20 mb-8 sm:mt-16 sm:mb-16 xl:mt-0 xl:m pt-[2rem]  md:pt-[12vw] lg:pt-0"
       id="features"
-      
       style={{
-        //backgroundImage: "linear-gradient(to bottom, #140836 10%, #26272b)", // Define your gradient colors here
-        backgroundImage:   `url(${background1})`,        
-        backgroundSize: 'cover', // Set the background size to cover the section
+        backgroundImage: `url(${background1})`,
+        backgroundSize: 'cover',
         backgroundPosition: 'center',
-        margin: 0, // Ensure zero margin
-        padding: 0, // Ensure zero padding
-        // Define your gradient colors here
+        margin: 0,
+        padding: 0,
       }}
-      
-
     >
-      
-
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true }}
         transition={{ duration: 0.5, delay: 0.2 }}
-      >        
-        <div className="flex flex-wrap items-center 2xl:w-[1450px] xl:w-[1300px] w-11/12 mx-auto md:pl-4 xl:pr-16 xl:pl-16">
-          
+      >
+      <div className="flex flex-wrap items-center justify-center 2xl:w-[1450px] xl:w-[1300px] w-11/12 mx-auto md:pl-4 xl:pr-16 xl:pl-16">
+        <div className="w-full lg:w-3/4 mb-12 lg:mb-0">
+          <div className="mx-auto lg:mb-0 w-95 sm:w-4/5 md:w-95 lg:w-unset text-center">
+            <h2 className="text-xl lg:text-2xl font-bold mb-4 " style={{ color: "#DCA3EF" }}>Embrace Innovation</h2>
+            <h2 className="mt-6 mb-4 text-4xl lg:text-5xl custom-block-big-title">
+              Unlike any blockchain
+            </h2>
+            <h3 className="mt-6 mb-4 text-4xl lg:text-5xl titulofade">
+              You Have Used Before
+            </h3>
+            <p className="mb-8 text-white text-center text-sm lg:text-base md:w-11/12 lg:w-3/4 mx-auto">
+                Metchain functions as a decentralized blockchain
+                platform, efficiently handling significant
+                transaction volumes for real-time experiences
+                via a customized KHeavyhash. The platform's
+                objective is to promote coexistence, creation,
+                and interaction in virtual worlds while supporting
+                NFTs and fostering decentralized Metaverse
+                interoperability and economy. With established
+                Layer 1 and Layer 2 blockchains, Metchain is
+                advancing towards its Layer 3 goals,
+                emphasizing its role in handling high transaction
+                volumes for real-time experiences and contributing
+                to Metaverse interoperability.     
+              </p>
+              <div className="mb-8 lg:mb-0 px-2 flex justify-center flex-col lg:flex-row">
+                <div className="mr-8 flex items-center">
+                  <CheckArrowIcon />
+                  <span className="ml-2 text-white">Layer 1</span>
+                </div>
+                <div className="mr-8 flex items-center">
+                  <CheckArrowIcon />
+                  <span className="ml-2 text-white">Layer 2</span>
+                </div>
+                <div className="flex items-center">
+                  <LoadingIcon />
+                  <span className="ml-2 text-white">Layer 3</span>
+                </div>
+              </div>
+          </div>
 
-            <div className="w-full lg:w-1/2 mb-12 lg:mb-0">
-              <div className="mx-auto lg:mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-unset">
-                <span className="custom-block-subtitle">Embrace Innovation</span>
-                <h2 className="mt-6 mb-8 text-4xl lg:text-5xl custom-block-big-title">
-                  Unlike any blockchain you have used before
-                </h2>
-                <p className="mb-10 text-white leading-loose">
-                  Metchain functions as a decentralized blockchain
-                  platform, efficiently handling significant
-                  transaction volumes for real-time experiences
-                  via a customized KHeavyhash. The platform's
-                  objective is to promote coexistence, creation,
-                  and interaction in virtual worlds while supporting
-                  NFTs and fostering decentralized Metaverse
-                  interoperability and economy. With established
-                  Layer 1 and Layer 2 blockchains, Metchain is
-                  advancing towards its Layer 3 goals,
-                  emphasizing its role in handling high transaction
-                  volumes for real-time experiences and contributing
-                  to Metaverse interoperability.     
-                </p>
-                <ul className="mb-6 text-white">
-                  <li className="mb-4 flex">
-                    <CheckArrowIcon />
-                    <span>Layer 1</span>
-                  </li>
-                  <li className="mb-4 flex">
-                    <CheckArrowIcon />
-                    <span>Layer 2</span>
-                  </li>
-                  <li className="mb-4 flex">
-                    <LoadingIcon />
-                    <span>Layer 3</span>
-                  </li>
-                </ul>
-                
-              </div>
-            </div>
-         
-          <div className="w-3/4 mx-auto lg:w-1/2 flex flex-wrap lg:-mx-4 sm:pr-8 lg:pt-10 justify-center lg:pl-4 xl:px-8">
-            <div className="mb-8 lg:mb-0 w-full sm:w-1/2 px-2 lg:px-0">
-              <div className="mb-4 py-3 pl-3 pr-2 rounded">
-                <img
-                  src={feature1}
-                  alt="f1"
-                  className="rounded-xl  custom-border-gray mx-auto sm:mx-unset"
-                />
-              </div>
-              <div className="py-3 pl-3 pr-2 rounded ">
-                <img
-                  src={feature2}
-                  alt="f2"
-                  className="rounded-xl  custom-border-gray mx-auto sm:mx-unset"
-                />
-              </div>
-            </div>
-            <div className="w-1/2 lg:mt-20  pt-12 lg:pt-0 px-2 hidden sm:inline-block">
-              <div className="mb-4 py-3 pl-3 pr-2 rounded-lg ">
-                <img
-                  src={feature3}
-                  alt="f3"
-                  className="rounded-xl  custom-border-gray"
-                />
-              </div>
+      
 
+          <div className="mb-8 lg:mb-0 px-2 flex justify-center flex-col lg:flex-row">
+            <div className="mb-4 lg:mb-0 py-2 px-3 rounded-lg">
+              <img
+                src={feature1}
+                alt="f1"
+                className="w-full h-auto rounded-xl custom-border-gray"
+              />
+            </div>
+            <div className="mb-4 lg:mb-0 py-2 px-3 rounded-lg">
+              <img
+                src={feature2}
+                alt="f2"
+                className="w-full h-auto rounded-xl custom-border-gray"
+              />
+            </div>
+            <div className="py-2 px-3 rounded-lg">
+              <img
+                src={feature3}
+                alt="f3"
+                className="w-full h-auto rounded-xl custom-border-gray"
+              />
             </div>
           </div>
         </div>
+      </div>
       </motion.div>
     </section>
   );
